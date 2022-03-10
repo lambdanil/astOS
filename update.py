21156ceaba9d0fd185a1bdb0b36fad6d403f146c
CuBeRJAN <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-10 23:25:18 +0000

    Add files via upload

--- update.py
+++ update.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 import os
 
-USER = "jan"
-
-
-#os.system("sudo -u jan DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send astUpdate 'System upgrade is being installed...' -u critical")
-
 while True:
     if os.path.exists("/var/astpk/lock"):
         time.sleep(20)
@@ -17,8 +12,6 @@ upstate = open("/var/astpk/upstate")
 line = upstate.readline()
 upstate.close()
 if "0" in line:
-#    os.system("sudo -u jan DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send astUpdate 'System has been upgraded succesfully' -u critical")
     os.system("/usr/bin/ast deploy $(ast c)")
 else:
-#    os.system("sudo -u jan DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send astUpdate 'System upgrade failed' -u critical")
     print()
