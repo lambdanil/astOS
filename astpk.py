1626fae296d2d93d8d435ff806d506bc151941f2
CuBeRJAN <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-19 20:27:07 +0100

    Add files via upload

--- astpk.py
+++ astpk.py
@@ -229,7 +229,7 @@ def deploy(overlay):
         os.system(f"cp --reflink=auto -r /.var/var-{etc}/lib/pacman/* /var/lib/pacman/ >/dev/null 2>&1") # Copy pacman and systemd directories
         os.system(f"cp --reflink=auto -r /.var/var-{etc}/lib/systemd/* /var/lib/systemd/ >/dev/null 2>&1")
         os.system(f"btrfs sub set-default /.overlays/overlay-{tmp}") # Set default volume
-        print(tmp)
+        os.system(f"chattr -RV +i /.overlays/overlay-{tmp}/usr > /dev/null 2>&1")
         print(f"{overlay} was deployed to /")
 
 # Add node to branch
@@ -509,9 +509,11 @@ def live_install(pkg):
 # Live unlocked shell
 def live_unlock():
     tmp = get_tmp()
+    os.system(f"chattr -RV -i /.overlays/overlay-{tmp}/usr > /dev/null 2>&1")
     os.system(f"mount --bind /.overlays/overlay-{tmp} /.overlays/overlay-{tmp}")
     os.system(f"arch-chroot /.overlays/overlay-{tmp}")
     os.system(f"umount /.overlays/overlay-{tmp}")
+    os.system(f"chattr -RV +i /.overlays/overlay-{tmp}/usr > /dev/null 2>&1")
 
 # Install packages
 def install(overlay,pkg):
