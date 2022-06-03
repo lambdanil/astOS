40af98e6af24844789d398763e46a3f54cb850c7
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-06-03 09:56:10 +0200

    small changes to update script

--- update.py
+++ update.py
@@ -3,17 +3,18 @@ import os
 import time
 import subprocess
 
-snapshot = subprocess.check_output("/usr/bin/ast c", shell=True)
+snapshot = subprocess.check_output("/usr/local/sbin/ast c", shell=True)
 while True:
-    if os.path.exists(f"/.snapshots/snapshot-chr{snapshot}"):
+    if os.path.exists(f"/.snapshots/rootfs/snapshot-chr{snapshot}"):
         time.sleep(20)
     else:
-        os.system("/usr/bin/ast clone $(/usr/bin/ast c)")
-        os.system("/usr/bin/ast auto-upgrade")
+        os.system("/usr/local/sbin/ast clone $(/usr/local/sbin/ast c)")
+        os.system("/usr/local/sbin/ast auto-upgrade")
+        os.system("/usr/local/sbin/ast base-update")
         break
 
 upstate = open("/var/astpk/upstate")
 line = upstate.readline()
 upstate.close()
 if "1" not in line:
-    os.system("/usr/bin/ast deploy $(/usr/bin/ast c)")
+    os.system("/usr/local/sbin/ast deploy $(/usr/local/sbin/ast c)")
