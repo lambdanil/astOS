1ac4f038f1d7c5b8b83983bae3651b696a07176a
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-08-16 16:22:25 +0200

    fix indent

--- astpk.py
+++ astpk.py
@@ -1008,10 +1008,10 @@ def main(args):
         if len(args) > 3:
             if args[2] == "--not-live":
                 live = False
-    if not live:
-        sync_tree(fstree,args[args.index(arg)+2],False,live)
-    else:
-        sync_tree(fstree,args[args.index(arg)+1],False,live)
+        if not live:
+            sync_tree(fstree,args[args.index(arg)+2],False,live)
+        else:
+            sync_tree(fstree,args[args.index(arg)+1],False,live)
     elif arg == "fsync" or arg == "force-sync":
         live = True
         if len(args) > 3:
